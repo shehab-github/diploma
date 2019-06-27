dc68e6b973624693a539dc9760491aa64e3b4365
Shehab <ShehabElDabaa@users.noreply.github.com>
2019-06-27 22:15:35 +0400

    Update def.js
    fixing url concat issue of string

--- def.js
+++ def.js
@@ -6,7 +6,7 @@ $( "#btn_Login" ).click(function( event ) {
 	var userName = $("#txt_userName").val();
 	var password = $("#txt_password").val();
 
- var url = window.location.protocol + "///" window.location.host + "//" + window.location.pathname.split('/')[0];
+ var url = window.location.protocol + "//" + window.location.host + "//" + window.location.pathname.split('/')[0];
  
  if(userName == "worker" || userName == "driver"){
 	 
